a3a2c98e62058b67d480b25a9908b8bff66c70f3
Jonathan Martz <git@jmartz.de>
2022-06-03 13:54:56 +0200

    add plausible

--- app/src/main.js
+++ app/src/main.js
@@ -4,12 +4,18 @@ import App from './App.vue'
 import './tailwind.css'
 import routes from './routes'
 import i18n from './translate'
+import plausible from './plugins/plausible';
 
 const app = createApp(App);
 
-
+const plausibleOptions = { 
+  domain: 'www.jmartz.de',
+  apiHost: 'https://tracking.jmartz.gmbh',
+  hashMode: false,
+  trackLocalhost: false,
+};
   
-
+app.use(plausible,plausibleOptions);
 app.use(i18n);
 app.use(routes);
 app.mount('#app');
